22f178e356cc7adad7881ac45dd330bb9bf5cfe9
smhtdf <smhtdf@gmail.com>
2014-04-01 22:00:04 +0100

    Initial commit

---
+++ TitaniumDragonfly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TitaniumDragonfly
+//
+//  Created by Current User on 01/04/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDFAppDelegate class]));
+    }
+}
---
+++ TitaniumDragonflyTests/TitaniumDragonflyTests.m
@@ -0,0 +1,34 @@
+//
+//  TitaniumDragonflyTests.m
+//  TitaniumDragonflyTests
+//
+//  Created by Current User on 01/04/2014.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TitaniumDragonflyTests : XCTestCase
+
+@end
+
+@implementation TitaniumDragonflyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
