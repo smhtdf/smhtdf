ae49783d3459f45dfbfe0139ead2b7e8cba51f9e
smhtdf <smhtdf@gmail.com>
2014-04-02 07:03:38 +0100

    Added lookMap (MKMapView) to the Look view controller.

---
+++ TitaniumDragonfly/TDFLookViewController.h
@@ -0,0 +1,16 @@
+//
+//  TDFLookViewController.h
+//  TitaniumDragonfly
+//
+//  Created by Current User on 02/04/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface TDFLookViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet MKMapView *lookMap;
+
+@end
