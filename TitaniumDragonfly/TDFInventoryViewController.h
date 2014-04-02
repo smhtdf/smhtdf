43018cb47e4ce694241938b855a8d7bad556b0b3
smhtdf <smhtdf@gmail.com>
2014-04-02 10:48:56 +0100

    Populate inventory table view from Parse class InventoryItem.

---
+++ TitaniumDragonfly/TDFInventoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  TDFInventoryViewController.h
+//  TitaniumDragonfly
+//
+//  Created by Current User on 02/04/2014.
+//
+//
+
+#import <Parse/Parse.h>
+
+@interface TDFInventoryViewController : PFQueryTableViewController
+
+@end
---
+++ TitaniumDragonfly/TDFInventoryViewController.m
@@ -0,0 +1,65 @@
+//
+//  TDFInventoryViewController.m
+//  TitaniumDragonfly
+//
+//  Created by Current User on 02/04/2014.
+//
+//
+
+#import "TDFInventoryViewController.h"
+
+@interface TDFInventoryViewController ()
+
+@end
+
+@implementation TDFInventoryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - Parse API
+
+- (PFQuery *)queryForTable {
+    PFQuery *query = [PFQuery queryWithClassName:self.parseClassName];
+    
+    // If no objects are loaded in memory, we look to the cache first to fill the table
+    // and then subsequently do a query against the network.
+    if (self.objects.count == 0) {
+        query.cachePolicy = kPFCachePolicyCacheThenNetwork;
+    }
+    
+    [query orderByDescending:@"createdAt"];
+    
+    return query;
+}
+
+@end
