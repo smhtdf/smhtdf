d299dd4cbdb1703c212e5893174d975f22752270
smhtdf <smhtdf@gmail.com>
2014-04-06 10:20:12 +0100

    Added loadView test for TDFCollectItemViewController.

---
+++ TitaniumDragonflyTests/TDFCollectItemViewControllerTests.m
@@ -0,0 +1,40 @@
+//
+//  TDFCollectItemViewControllerTests.m
+//  TitaniumDragonfly
+//
+//  Created by Current User on 06/04/2014.
+//
+//
+
+#import "TDFCollectItemViewController.h"
+#import <XCTest/XCTest.h>
+
+@interface TDFCollectItemViewControllerTests : XCTestCase {
+    TDFCollectItemViewController *_collectItemViewController;
+}
+
+@end
+
+@implementation TDFCollectItemViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    _collectItemViewController = [TDFCollectItemViewController new];
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)test_loadView_view_isNotNull
+{
+    [_collectItemViewController loadView];
+    XCTAssertNotNil(_collectItemViewController.view, @"The view should have loaded!");
+}
+
+@end
