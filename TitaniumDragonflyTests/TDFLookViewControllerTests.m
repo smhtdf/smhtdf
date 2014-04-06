4d4e5255d0da97ec273ae3d8c82a9f29137ee943
smhtdf <smhtdf@gmail.com>
2014-04-07 00:52:38 +0100

    see bugzid:315

--- TitaniumDragonflyTests/TDFLookViewControllerTests.m
+++ TitaniumDragonflyTests/TDFLookViewControllerTests.m
@@ -53,6 +53,7 @@
 - (void)test_locationDidChange_lookMap_setRegion
 {
     // Line to test GitHub integration with FogBugz
+    // using See bugzid:315
     // Create the mock map
     id lookMapMock = [OCMockObject mockForClass:[MKMapView class]];
     
